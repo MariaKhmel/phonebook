817aa929032d063625feafcc9b682edc25368cdb
Maria Khmel <mariakhmel8@gmail.com>
2025-02-06 00:58:25 +0200

    form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,18 @@
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
-import contacts from "../../data/contacts";
+import initialContacts from "../../data/contacts";
+import { useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState(initialContacts);
+
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,14 @@
-const ContactList = () => {
-  return <></>;
+import Contact from "../Contact/Contact";
+
+const ContactList = ({ contacts }) => {
+  return (
+    <ul>
+      {contacts.map((contact) => (
+        <li key={contact.id}>
+          <Contact contact={contact} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 export default ContactList;

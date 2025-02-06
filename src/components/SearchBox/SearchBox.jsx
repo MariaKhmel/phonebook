c3d3c7ee1ca0651d15ab929779c47274730c373c
Maria Khmel <mariakhmel8@gmail.com>
2025-02-07 01:55:55 +0200

    filter

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,16 @@
-const SearchBox = () => {
-  return <></>;
+const SearchBox = ({ filterValue, onFilterChange }) => {
+  return (
+    <div>
+      <label htmlFor="search">Find contacts by name</label>
+      <input
+        type="text"
+        id="search"
+        name="input"
+        value={filterValue}
+        onChange={(e) => onFilterChange(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default SearchBox;

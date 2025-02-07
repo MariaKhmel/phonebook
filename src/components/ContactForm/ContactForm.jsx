7886a084b1aaf806d98d56f9072cd6582694a09c
Maria Khmel <mariakhmel8@gmail.com>
2025-02-07 23:47:21 +0200

    form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,12 @@
 const ContactForm = () => {
-  return <></>;
+  return (
+    <form>
+      <label>Name</label>
+      <input type="text" />
+      <label>Number</label>
+      <input type="text" />
+      <button>Add contact</button>
+    </form>
+  );
 };
 export default ContactForm;
